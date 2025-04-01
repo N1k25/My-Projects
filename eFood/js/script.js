77ca0aa7acd2b99334cc39b29c47982915f82aa5
N1k25 <grushko_nikolai@ukr.net>
2025-04-01 14:13:39 +0300

    Create section Reviews

--- eFood/js/script.js
+++ eFood/js/script.js
@@ -22,5 +22,28 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('.reviews-slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:1,
+		autoplay: false,
+		infinite: false,
+		speed:500,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
 
