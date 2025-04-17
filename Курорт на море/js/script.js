50a03b91dd4e64faab43618cae119299163fe235
N1k25 <grushko_nikolai@ukr.net>
2025-04-17 12:31:43 +0300

    Create section Reviews

--- Курорт на море/js/script.js
+++ Курорт на море/js/script.js
@@ -1,26 +1,4 @@
 $(document).ready(function(){
-	// $('.slider').slick({
-	// 	arrows: true,
-	// 	dots:true,
-	// 	slidesToShow:1,
-	// 	autoplay:false,
-	// 	speed:500,
-	// 	autoplaySpeed:800,
-	// 	responsive:[
-	// 		{
-	// 			breakpoint: 768,
-	// 			settings: {
-	// 				slidesToShow:2
-	// 			}
-	// 		},
-	// 		{
-	// 			breakpoint: 550,
-	// 			settings: {
-	// 				slidesToShow:1
-	// 			}
-	// 		}
-	// 	]
-	// });
 	let $slider = $('.slider');
 	let $counter = $('.slider-counter');
 
