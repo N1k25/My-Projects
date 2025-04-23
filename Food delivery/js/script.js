7dcce156a203e894fe29de82ec01683161f0da54
N1k25 <grushko_nikolai@ukr.net>
2025-04-23 13:42:06 +0300

    Create section Specialities

--- Food delivery/js/script.js
+++ Food delivery/js/script.js
@@ -21,5 +21,27 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('.specialities-slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:8,
+		autoplay:false,
+		speed:500,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
 
