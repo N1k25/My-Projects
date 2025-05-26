dbdde33e3e840dc3d5f1dd81c00d455ce240e1e6
N1k25 <grushko_nikolai@ukr.net>
2025-05-26 12:56:35 +0300

    Create section We know

---
+++ Secfi/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('.we_know-slider').slick({
+		arrows:false,
+		dots:true,
+		slidesToShow:1,
+		autoplay:false,
+		speed:500,
+		autoplaySpeed:800,
+		// responsive:[
+		// 	{
+		// 		breakpoint: 768,
+		// 		settings: {
+		// 			slidesToShow:2
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 550,
+		// 		settings: {
+		// 			slidesToShow:1
+		// 		}
+		// 	}
+		// ]
+	});
+});
+
