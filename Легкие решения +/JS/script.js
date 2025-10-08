141be24b7264d58269c488872d5484ba9c90c18c
N1k25 <grushko_nikolai@ukr.net>
2025-10-08 12:14:19 +0300

    Create section Subscription

--- Легкие решения +/JS/script.js
+++ Легкие решения +/JS/script.js
@@ -3,3 +3,15 @@ const buttonVideoPlay = document.getElementById('video-play__btn');
 buttonVideoPlay.addEventListener('click', function() {
     alert('Воспроизведение видео!');
 });
+
+
+const subscriptionInput = document.getElementById('subscription-input');
+const formButton = document.getElementById('form-button');
+
+formButton.addEventListener('click', function() {
+    if (subscriptionInput.value !== '') {
+        alert('Форма отправлена!');
+    } else {
+        alert('Введите e-mail!');
+    }
+});
