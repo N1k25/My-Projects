db5d5c5a100b9d4c5b8e0934460564136303c2a0
N1k25 <grushko_nikolai@ukr.net>
2025-10-09 13:48:19 +0300

    Project Completed

--- Легкие решения +/JS/script.js
+++ Легкие решения +/JS/script.js
@@ -1,7 +1,9 @@
-const buttonVideoPlay = document.getElementById('video-play__btn');
+const buttonVideoPlay = document.getElementsByClassName('video-play__btn');
+const videoFrame = document.getElementById('video__frame');
 
-buttonVideoPlay.addEventListener('click', function() {
-    alert('Воспроизведение видео!');
+buttonVideoPlay[0].addEventListener('click', function() {
+    buttonVideoPlay[0].classList.add('none');
+    videoFrame.classList.remove('none');
 });
 
 
@@ -9,9 +11,22 @@ const subscriptionInput = document.getElementById('subscription-input');
 const formButton = document.getElementById('form-button');
 
 formButton.addEventListener('click', function() {
-    if (subscriptionInput.value !== '') {
-        alert('Форма отправлена!');
-    } else {
+    // Переменная для валидации (есть ли ошибка валидации)
+    let validationResult = false;      
+    
+    if (subscriptionInput.value === "") {
         alert('Введите e-mail!');
+        validationResult = true;
+    }   
+    if (subscriptionInput.value.length <= 6 || subscriptionInput.value.includes("@") === false || subscriptionInput.value.includes(".") === false) {
+        alert("Введите правильную почту!");
+        validationResult = true;
     }
+
+    if (validationResult === true) {
+        return;
+    }  
+
+    alert('Регистрация прошла успешно!');
+    subscriptionInput.value = '';
 });
