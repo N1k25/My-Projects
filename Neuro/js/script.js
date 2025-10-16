315522febeec4d048526b3f28e0270468e5e6c50
N1k25 <grushko_nikolai@ukr.net>
2025-10-16 12:20:52 +0300

    Create section Hero

--- Neuro/js/script.js
+++ Neuro/js/script.js
@@ -7,4 +7,21 @@ menuShopIcon.addEventListener("click", function() {
     menuShopCounter.textContent = '0';
     counter = 0;
 });
-  
+
+
+const shopButtons = document.querySelectorAll('.shop-btn');
+
+shopButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    counter ++;
+    menuShopCounter.textContent = counter;
+  });
+});
+
+
+const cookiesBlock = document.querySelector('.cookies-block');
+const cookiesButton = document.querySelector('.cookies-btn');
+
+cookiesButton.addEventListener('click', function() {
+    cookiesBlock.classList.add('none');
+});
