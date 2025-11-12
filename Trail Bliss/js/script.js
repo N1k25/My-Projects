54a3a7e8e3e70a9d1676f7e5d9d82ee09394d95e
N1k25 <grushko_nikolai@ukr.net>
2025-11-12 13:54:58 +0200

    Create section Partner

--- Trail Bliss/js/script.js
+++ Trail Bliss/js/script.js
@@ -21,5 +21,27 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('.partner-slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:2,
+		autoplay:false,
+		speed:500,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
 
