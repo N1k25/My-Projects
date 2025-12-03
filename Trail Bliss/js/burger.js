cce800bed096256d8fe285aa7914df6dae2852fe
N1k25 <grushko_nikolai@ukr.net>
2025-12-03 16:47:23 +0200

    Mobile version added and Project Completed

---
+++ Trail Bliss/js/burger.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        mobileMenu.classList.toggle("active");
+    });
+});
