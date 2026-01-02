59d424451514b146220617883791f201693bf0d5
N1k25 <grushko_nikolai@ukr.net>
2026-01-02 18:59:46 +0200

    Create section Menu

--- Takeaway-Delivery/js/script.js
+++ Takeaway-Delivery/js/script.js
@@ -1,5 +1,13 @@
 let counter = document.getElementById('counter');
 
+const orderButtons = document.querySelectorAll('.order-btn');
+orderButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        let currentCount = parseInt(counter.textContent);
+        counter.textContent = currentCount + 1;
+    });
+});
+
 let shoppingCart = document.querySelector('.shopping-cart');
 shoppingCart.addEventListener('click', function() {
     counter.textContent = '0';
