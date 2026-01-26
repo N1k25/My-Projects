4081682ac1f4ab5d0e110e4ad02ec923d2ab5320
N1k25 <grushko_nikolai@ukr.net>
2026-01-26 16:59:03 +0200

    Create modal window About

---
+++ Worktap/js/modal.js
@@ -0,0 +1,25 @@
+// Открыть модальное окно
+document.getElementById("open-modal__about-btn").addEventListener("click", function() {
+    document.getElementById("modal-about").classList.add("open")
+})
+
+// Закрыть модальное окно
+document.getElementById("close-modal__about-btn").addEventListener("click", function() {
+    document.getElementById("modal-about").classList.remove("open")
+})
+
+// Закрыть модальное окно при нажатии на Esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("modal-about").classList.remove("open")
+    }
+});
+
+// Закрыть модальное окно при клике вне его
+document.querySelector("#modal-about .modal__box-about").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("modal-about").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+});
