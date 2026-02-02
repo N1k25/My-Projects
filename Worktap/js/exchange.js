71beb1eab7fa70a4fd22d7822493aeffd39f6bb7
N1k25 <grushko_nikolai@ukr.net>
2026-02-02 12:21:11 +0200

    Added a change to the project sorting text in the section Projects on the page Exchange

--- Worktap/js/exchange.js
+++ Worktap/js/exchange.js
@@ -14,3 +14,14 @@ if (this.value === 'redirect') {
 }
 });
 
+
+// Изменение текста сортировки проектов в зависимости от выбора пользователя
+document.getElementById('projects-sort-select').addEventListener('change', function () {
+if (this.value === 'min-max-price') {
+    document.querySelector('.projects__exchange-sort--select-label-start-price').textContent = 'Минимальная цена';
+    document.querySelector('.projects__exchange-sort--select-label-end-price').textContent = 'Максимальная цена';
+} else if (this.value === 'max-min-price') {
+    document.querySelector('.projects__exchange-sort--select-label-start-price').textContent = 'Максимальная цена';
+    document.querySelector('.projects__exchange-sort--select-label-end-price').textContent = 'Минимальная цена';
+}
+});
