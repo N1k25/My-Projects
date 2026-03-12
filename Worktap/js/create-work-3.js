190ee8f96248864de9db328d8a9a4d71f3ef5420
N1k25 <grushko_nikolai@ukr.net>
2026-03-12 14:22:34 +0200

    Created page Create Work (Step 3)

---
+++ Worktap/js/create-work-3.js
@@ -0,0 +1,17 @@
+// Для поля ввода работы добавляем обработчик события ввода и проверяем, заполнено ли оно
+const workEditor = document.getElementById('work__editor');
+workEditor.addEventListener('input', () => {
+    if (workEditor.value.trim() !== '') {
+        buttonNext.classList.remove('step__button--disabled');
+    } else {
+        buttonNext.classList.add('step__button--disabled');
+    }
+});
+
+// Для кнопок редактирования текста добавляем обработчик события клика и переключаем класс selected
+const editorButtons = document.querySelectorAll('.work__editor-header-btn--icon');
+editorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('selected');
+    });
+});
