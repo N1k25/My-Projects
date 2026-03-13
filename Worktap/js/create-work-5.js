06e2c1fe1e8063e1aed12568bfa738ddaa3a1a24
N1k25 <grushko_nikolai@ukr.net>
2026-03-13 16:01:51 +0200

    Created page Create Work (Step 5)

---
+++ Worktap/js/create-work-5.js
@@ -0,0 +1,68 @@
+// Добавление новой фотографии
+const addNewPhotoButton = document.getElementById('add__new--photo');
+addNewPhotoButton.addEventListener('click', () => {
+    buttonNext.classList.remove('step__button--disabled');
+});
+
+
+// Ссылка на YouTube
+const linkYoutubeInput = document.getElementById('link_youtube_input');
+linkYoutubeInput.addEventListener('input', () => {
+    if (linkYoutubeInput.value.trim() !== '') {
+        buttonNext.classList.remove('step__button--disabled');
+    } else {
+        buttonNext.classList.add('step__button--disabled');
+    }
+});
+
+
+// Загрузка документов
+const uploadBtn = document.querySelector('.gallery__documents-form--input--field-download-btn');
+const fileInput = document.querySelector('.gallery__documents-file-input');
+const fileList = document.querySelector('.gallery__documents-form--list');
+
+uploadBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    fileInput.click();
+});
+
+fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+
+    if (!file) return;
+
+    const li = document.createElement('li');
+    li.className = 'gallery__documents-form--list-item';
+    li.innerHTML = `
+        <div class="gallery__documents-form--list-item-file--name">${file.name}</div>
+        <div class="gallery__documents-form--list-item-file-service">
+            <button type="button" class="gallery__documents-form--list-item-file-service-btn">
+                <img src="./create_work/img/page_down.svg" class="gallery__documents-form--list-item-file-service-icon">
+            </button>
+            <div class="gallery__documents-form--list-item-file-status file-status__not--completed">
+                <img src="./create_work/img/done.svg" alt="Загруженный файл" class="gallery__documents-form--list-item-file-status-icon">
+            </div>
+        </div>
+    `;
+
+    fileList.appendChild(li);
+
+    fileInput.value = '';
+
+    buttonNext.classList.remove('step__button--disabled');
+});
+
+
+// Добавление обработчика события клика на список документов (для переключения статуса загруженного файла)
+const documentList = document.querySelector('.gallery__documents-form--list');
+
+documentList.addEventListener('click', function (e) {
+	const button = e.target.closest('.gallery__documents-form--list-item-file-service-btn');
+
+	if (!button) return;
+
+	const documentListItem = button.closest('.gallery__documents-form--list-item');
+	const documentStatus = documentListItem.querySelector('.gallery__documents-form--list-item-file-status');
+
+	documentStatus.classList.toggle('file-status__not--completed');
+});
