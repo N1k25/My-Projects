3fd4494addfebbb51d8b5c4f82530010af7ad04f
N1k25 <grushko_nikolai@ukr.net>
2026-03-30 13:19:47 +0300

    fixed navigation in menu

--- Worktap/js/profile.js
+++ Worktap/js/profile.js
@@ -1,3 +1,20 @@
+// Получаем элементы радиокнопок и ссылку на конкурсы
+const userRoleCustomer = document.getElementById('role-customer');
+const userRolePerformer = document.getElementById('role-performer');
+const contestLink = document.getElementById('contest-link');
+
+if (userRoleCustomer.checked) {
+    contestLink.href = "./contest_freelancer.html";
+};
+
+userRolePerformer.addEventListener('change', () => {
+    if (userRolePerformer.checked) {
+        contestLink.href = "./contest_creator.html";
+    }
+});
+
+
+// Получаем элементы для раскрывающегося блока с подробной информацией
 const profileDetails = document.querySelector('.hero__profile-details');
 const profileDetailsInfoTitle = document.getElementById('hero__profile-details-info-title');
     
