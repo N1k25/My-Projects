58a82397000dffb07b961290d41098a12836c1f0
N1k25 <grushko_nikolai@ukr.net>
2026-04-16 13:39:24 +0300

    Create section Stock

--- GGPromarket/js/app.js
+++ GGPromarket/js/app.js
@@ -1,7 +1,10 @@
 const tabsBtn   = document.querySelectorAll(".tabs__nav-btn");
+const stockTabsBtn   = document.querySelectorAll(".stock--tabs__nav-btn");
 const tabsItems = document.querySelectorAll(".tabs__item");
+const stockTabsItems = document.querySelectorAll(".stock-tabs__item");
 
 tabsBtn.forEach(onTabClick);
+stockTabsBtn.forEach(onStockTabClick);
 
 function onTabClick(item) {
     item.addEventListener("click", function() {
@@ -24,6 +27,26 @@ function onTabClick(item) {
     });
 }
 
+function onStockTabClick(item) {
+    item.addEventListener("click", function() {
+        let stockCurrentBtn = item;
+        let stockTabId = stockCurrentBtn.getAttribute("stock-data-tab");
+        let stockCurrentTab = document.querySelector(stockTabId);
+
+        if( ! stockCurrentBtn.classList.contains('active') ) {
+            stockTabsBtn.forEach(function(item) {
+                item.classList.remove('active');
+            });
+            
+            stockTabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            stockCurrentBtn.classList.add('active');
+            stockCurrentTab.classList.add('active');
+        }
+    });
+}
+
 document.querySelector('.tabs__nav-btn').click();
-
-
+document.querySelector('.stock--tabs__nav-btn').click();
--- GGPromarket/js/slider.js
+++ GGPromarket/js/slider.js
@@ -43,5 +43,26 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('.stock-slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:5,
+		autoplay:false,
+		speed:500,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
-
