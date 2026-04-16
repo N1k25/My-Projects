ee5406d8842ad70dbc24234d27738db267318f7b
N1k25 <grushko_nikolai@ukr.net>
2026-04-16 16:48:39 +0300

    Create section Articles

--- GGPromarket/js/slider.js
+++ GGPromarket/js/slider.js
@@ -65,4 +65,26 @@ $(document).ready(function(){
 			}
 		]
 	});
+	$('.articles-slider').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:4,
+		autoplay:false,
+		speed:500,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
 });
