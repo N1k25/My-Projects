4adacda630e1e36ce241f947c3b51857dd57ea1d
N1k25 <grushko_nikolai@ukr.net>
2026-06-11 11:22:47 +0300

    Create section Map

--- Двери новые/js/main.js
+++ Двери новые/js/main.js
@@ -13,3 +13,41 @@ resetButton.addEventListener('click', () => {
 
     updateTrack();
 });
+
+/*----------------------*/
+
+const mapInputField = document.querySelector('.map-form__input--fild');
+const mapButton = document.querySelector('.map-form__btn');
+
+mapInputField.addEventListener('input', function () {
+    // оставляем только цифры
+    let digits = this.value.replace(/\D/g, '');
+
+    // обязательно начинаем с 380
+    if (!digits.startsWith('380')) {
+        digits = '380';
+    }
+
+    // 380 + 9 цифр
+    digits = digits.substring(0, 12);
+
+    this.value = '+' + digits;
+});
+
+mapInputField.addEventListener('focus', function () {
+    if (!this.value) {
+        this.value = '+380';
+    }
+});
+
+mapButton.addEventListener('click', () => {
+    const maxLength = 13;
+
+    if (mapInputField.value.length < maxLength) {
+        alert('Введите номер в формате +380XXXXXXXXX');
+    }
+
+    if (mapInputField.value.trim() != '') {
+        mapInputField.value = ''
+    }
+});
