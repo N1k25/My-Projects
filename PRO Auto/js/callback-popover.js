e26f860d86c0b243b9ed6e9980f78031ab231c8d
N1k25 <grushko_nikolai@ukr.net>
2026-07-22 15:23:07 +0300

    fixed bugs in Header Menu

--- PRO Auto/js/callback-popover.js
+++ PRO Auto/js/callback-popover.js
@@ -1,3 +1,8 @@
+const callbackPopover = document.getElementById('callback-popover');
+const callbackSuccessPopover = document.getElementById('callback-success-popover');
+const callbackForm = document.getElementById("callbackForm");
+
+
 // Для поля телефону обмежити введення символів (цифри 0-9, знак +, дужки (), дефіс -)
 const phoneInput = document.getElementById("phone");
 
@@ -17,17 +22,54 @@ phoneInput.addEventListener("input", function () {
 });
 
 
-// Очищать все поля формы при нажатии на кнопку "ОТПРАВИТЬ"
-const callbackForm = document.getElementById("callbackForm");
-const callbackSubmitButton = document.getElementById("submitBtn");
-
-callbackSubmitButton.addEventListener("click", () => {
-    callbackForm.reset();
-});
-
 // Очищать все поля формы при нажатии на кнопку "X"
 const callbackCloseButton = document.getElementById("closeBtn");
 
 callbackCloseButton.addEventListener("click", () => {
     callbackForm.reset();
+});
+
+
+function showError(input, text) {
+  input.classList.add("invalid");
+
+  input.parentElement.querySelector(".error").textContent = text;
+}
+
+// Валидация формы
+callbackForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let valid = true;
+
+  document.querySelectorAll(".error").forEach((el) => (el.textContent = ""));
+  document
+    .querySelectorAll(".invalid")
+    .forEach((el) => el.classList.remove("invalid"));
+
+  const name = callbackForm.name;
+  const phone = callbackForm.phone;
+  const agree = document.getElementById("agree");
+
+  if (name.value.trim() === "") {
+    showError(name, "Поле обязательное для заполнения");
+    valid = false;
+  }
+
+  if (phone.value.trim() === "") {
+    showError(phone, "Поле обязательное для заполнения");
+    valid = false;
+  }
+
+  if (!agree.checked) {
+    document.querySelector(".checkbox-error").textContent =
+      "Необходимо согласиться с обработкой персональных данных";
+    valid = false;
+  }
+
+  if (valid) {
+    callbackForm.reset();
+    callbackPopover.hidePopover();
+    callbackSuccessPopover.showPopover();
+  }
 });
